4099966ebcf04ad78217fc28ce8966d71b7290e2
Linkai Ma <85125095+LinkaiMa@users.noreply.github.com>
2024-04-15 20:05:40 -0400

    do rounding for A only

--- SR_Richardson_iteration.py
+++ SR_Richardson_iteration.py
@@ -2,7 +2,7 @@ from SR_tools import *
 from tqdm import tqdm
 from sklearn.datasets import make_sparse_spd_matrix
 import os
-n = 100; steps = 2000; digits = 5; avg = 10; rate = 0.99; path = 'SR_no_avg'  # SR_no_avg
+n = 100; steps = 100; digits = 5; avg = 10; rate = 0.5; path = 'SR_only_A'  # SR_no_avg
 if not os.path.exists(path):
     os.makedirs(path)
 
@@ -13,26 +13,25 @@ A += np.eye(n)
 A0 = A.copy()
 x_sol = np.ones(shape=n)
 b = np.sum(A,axis=1)
-b0 = b.copy()
 
 rn_error = np.zeros(shape=steps)
 rn_error_avg = np.zeros(shape=steps)
 sr_error = np.zeros(shape=steps)
 sr_error_avg = np.zeros(shape=steps)
 x_rn = np.zeros(shape=n); x_sr = np.zeros(shape=n)
-A_rn = RN(A,digits); b_rn = RN(b,digits)
+A_rn = RN(A,digits); 
 x_vals_rn = np.zeros(shape=(n,steps))
 x_vals_sr = np.zeros(shape=(n,steps))
 
 for i in tqdm(range(steps)):
     rn_error[i] = np.linalg.norm(x_rn-x_sol) 
     sr_error[i] = np.linalg.norm(x_sr-x_sol)
-    x_rn = RN(x_rn+b_rn-A_rn@x_rn,digits)
+    x_rn = x_rn+b-A_rn@x_rn
     x_vals_rn[:,i] = x_rn
     x_rn_avg = np.average(x_vals_rn[:,i-avg+1:i+1],axis=1)
     rn_error_avg[i] = np.linalg.norm(x_rn_avg-x_sol)
-    A_sr = SR(A0,digits); b_sr = SR(b0,digits)
-    x_sr = SR(x_sr+b_sr-A_sr@x_sr,digits)
+    A_sr = SR(A0,digits); 
+    x_sr = x_sr+b-A_sr@x_sr
     x_vals_sr[:,i] = x_sr
     x_sr_avg = np.average(x_vals_sr[:,i-avg+1:i+1],axis=1)
     sr_error_avg[i] = np.linalg.norm(x_sr_avg-x_sol)
@@ -46,6 +45,7 @@ plt.ylabel('error')
 plt.title('Richardson convergence rate '+str(rate))
 plt.legend()
 plt.savefig(path+'/Richardson'+path+'.png')
+plt.show()
 plt.close()
 
 
